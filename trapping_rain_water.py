c9dac4dd82856167f0e09e57136918cf537b4b89
aldriechV <aldriechvillamor0922@gmail.com>
2025-11-07 12:56:24 -0800

    11/3 and 11/5 LC submission
    Implemented Solution for Trapping Rain Water + Two Sum 2

---
+++ trapping_rain_water.py
@@ -0,0 +1,33 @@
+"""
+
+Trapping Rain Water
+
+Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it can trap after raining.
+"""
+
+class Solution(object):
+    def trap(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        length = len(height)
+        left = [0] * length
+        right = [0] * length
+
+        #create my array
+        left[0] = height[0]
+        for i in range(1, length):
+            left[i] = max(left[i-1], height[i])
+
+        right[length-1] = height[length - 1]
+        for i in range(length-2, -1, -1):
+            right[i] =  max(right[i+1], height[i])
+        
+        #find the water
+        water = 0 
+        for i in range(length):
+            water += min(left[i], right[i]) - height[i]
+        
+        return water
+    
