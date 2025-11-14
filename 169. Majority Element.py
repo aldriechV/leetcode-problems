7365c3f501243f212321f68d539afac9ca4bd98b
aldriechV <aldriechvillamor0922@gmail.com>
2025-11-13 22:50:19 -0800

    Worked on Documentation

--- 169. Majority Element.py
+++ 169. Majority Element.py
@@ -5,6 +5,18 @@ Given an array nums of size n, return the majority element.
 
 The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
 
+Initial Approach:
+1. Create a hash map to count occurrences of each element.
+2. Iterate through the hash map to find the element with count greater than n/2.
+
+Optimized Approach:
+1. Keep a count and a target variable
+2. Since the value of the highest occuring element is more than half the array length,
+the highest occuring element will remain at the end if we increment count for the target
+3. Increment count for each duplicates and decrement for non-duplicates.
+4. return the final target value.
+
+
 """
 
 
--- 229. Majoirty Element II.py
+++ 229. Majoirty Element II.py
@@ -2,6 +2,17 @@
 229. Majority Element II
 
 Given an integer array of size n, find all elements that appear more than ⌊ n/3 ⌋ times.
+
+Initial Approach:
+1. Create a hash map to count occurrences of each element.
+2. Iterate through the hash map to find the element with count greater than n/2.
+
+Optimized Approach:
+1. Keep a count and a target variable
+2. Since the value of the highest occuring element is more than a third of the array length,
+the highest occuring element will remain at the end if we increment count for the target
+3. Increment count for each duplicates and decrement for non-duplicates.
+4. return the final target value.
 """
 
 
