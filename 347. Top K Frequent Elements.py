5575aaac71f32ea7479228cb33a99b679a413b55
aldriechV <aldriechvillamor0922@gmail.com>
2025-11-20 23:12:21 -0800

    Doxumentation added
    Notes added

--- 347. Top K Frequent Elements.py
+++ 347. Top K Frequent Elements.py
@@ -1,6 +1,11 @@
 """
 Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
 
+Solution
+1. Create a hash map to count occurrences of each element.
+2. Sort the hash map by values in descending order.
+3. Create a for loop that iterates through the first k elements of the sorted hash map and appends the keys to the final answer list.
+
 """
 
 class Solution(object):
