5c3e385e1dd2ad544e497cc15bfadf4b26aed0a7
aldriechV <aldriechvillamor0922@gmail.com>
2025-12-09 20:22:09 -0800

    Create 150. Evaluate Reverse Polish Notation.py

---
+++ 150. Evaluate Reverse Polish Notation.py
@@ -0,0 +1,43 @@
+"""
+Docstring for 150. Evaluate Reverse Polish Notation
+
+You are given an array of strings tokens that represents an arithmetic expression in a Reverse Polish Notation.
+
+Evaluate the expression. Return an integer that represents the value of the expression.
+"""
+
+
+class Solution(object):
+    def evalRPN(self, tokens):
+        """
+        :type tokens: List[str]
+        :rtype: int
+        """
+        stack = []
+        res = 0
+        for i in tokens:
+            if i == "/":
+                varOne = int(stack.pop())
+                varTwo = int(stack.pop())
+                res = float(varTwo) / varOne
+                stack.append(int(res))
+            elif i == "+":
+                varOne = stack.pop()
+                varTwo = stack.pop()
+                res = varTwo + varOne
+                stack.append(res)
+            elif i == "-":
+                varOne = stack.pop()
+                varTwo = stack.pop()
+                res = varTwo - varOne
+                stack.append(res)
+            elif i == "*":
+                varOne = stack.pop()
+                varTwo = stack.pop()
+                res = varTwo * varOne
+                stack.append(res)
+            else:
+                stack.append(int(i))
+        return stack[-1]
+    
+    
