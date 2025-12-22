6c813bd6bb932af374ebad00a93d03ee718e7926
aldriechV <aldriechvillamor0922@gmail.com>
2025-12-21 21:09:47 -0800

    Create 232. Implement Queue using Stacks.py

---
+++ 232. Implement Queue using Stacks.py
@@ -0,0 +1,54 @@
+"""
+Docstring for 232. Implement Queue using Stacks
+
+Implement a first in first out (FIFO) queue using only two stacks. The implemented queue should support all the functions of a normal queue (push, peek, pop, and empty).
+
+Implement the MyQueue class:
+
+void push(int x) Pushes element x to the back of the queue.
+int pop() Removes the element from the front of the queue and returns it.
+int peek() Returns the element at the front of the queue.
+boolean empty() Returns true if the queue is empty, false otherwise.
+"""
+
+class MyQueue(object):
+
+    def __init__(self):
+        self.stack = []
+        
+
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: None
+        """
+        self.stack.append(x)
+        
+
+    def pop(self):
+        """
+        :rtype: int
+        """
+        return self.stack.pop(0)
+
+    def peek(self):
+        """
+        :rtype: int
+        """
+        return self.stack[0]
+        
+
+    def empty(self):
+        """
+        :rtype: bool
+        """
+        return len(self.stack) == 0
+        
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
