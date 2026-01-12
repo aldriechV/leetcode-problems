4ad5b1e1eb090ec7b0c0f031ca1acc90672bbdb1
Aldriech Villamor <81332170+aldriechV@users.noreply.github.com>
2026-01-12 09:53:00 -0800

    Improve comments in Koko Eating Bananas solution
    Refactor comments for clarity and correctness.

--- 875. Koko Eating Bananas.py
+++ 875. Koko Eating Bananas.py
@@ -1,16 +1,10 @@
 """
 875. Koko Eating Bananas
 
-
 Koko loves to eat bananas. There are n piles of bananas, the ith pile has piles[i] bananas. The guards have gone and will come back in h hours.
-
 Koko can decide her bananas-per-hour eating speed of k. Each hour, she chooses some pile of bananas and eats k bananas from that pile. If the pile has less than k bananas, she eats all of them instead and will not eat any more bananas during this hour.
-
 Koko likes to eat slowly but still wants to finish eating all the bananas before the guards return.
-
 Return the minimum integer k such that she can eat all the bananas within h hours.
-
-
 """
 
 
@@ -23,7 +17,8 @@ class Solution(object):
         binary search between 0 and max(bananas)
         then the target 
         """
-        #math behind the problem, this works the problem out for us by keeping track of the hours 
+        # Computes the total number of hours needed if Koko eats at speed `value`.
+        # For each pile, we need ceil(pile / value) hours.
         def condition(value):
             total = 0
             for pile in piles:
@@ -31,7 +26,9 @@ class Solution(object):
             return total <= h
                 
 
-        #beecause the most amount of bananas we can eat is the highest number of bananas, we also want to account for the fact that we can only get one pile at a time, so anything past the max would not work/ 
+        #beecause the most amount of bananas we can eat is the highest number of bananas, we also want to account for the fact that we can only get one pile at a time, so anything past the max would not work/
+        # we're running a binary search where the values are from 0 to k, here k is the largest pile size
+        # we're looking to find a minimum which is why we are returning the left pointer
         left, right = 1, max(piles)
         while left < right:
             mid = left + (right - left) // 2
