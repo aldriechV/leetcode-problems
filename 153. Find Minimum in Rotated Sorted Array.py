3d3889c8aff2907cf521d5694f57efc34ad660db
Aldriech Villamor <81332170+aldriechV@users.noreply.github.com>
2026-01-19 10:45:24 -0800

    Add initial solution for minimum in rotated array
    Added initial solution outline for finding minimum in a rotated sorted array using binary search.

--- 153. Find Minimum in Rotated Sorted Array.py
+++ 153. Find Minimum in Rotated Sorted Array.py
@@ -9,6 +9,15 @@ Given the sorted rotated array nums of unique elements, return the minimum eleme
 
 You must write an algorithm that runs in O(log n) time.
 
+Initial Solution:
+1. Set up pointers necessary for binary search.
+2. Our challenge is not to find a number in the rotated array, but rather to find the minimum of the array. 
+3. We would want to binary search as normal, however we want to figure out where exactly our minimum is. 
+This would mean we need to find a way to move our low pointer to a minimum.
+4. Set the pointers so that when the medium is compared to the right pointer, we can determine where our minimum is
+    - If our medium is less than our high/right point, our minimum is closer to our medium, so we can set our lower pointer towards that medium
+    - If our medium is more than our high/right point, our minimum is closer to our right point, so we can move our higher pointer towards the medium. 
+
 """
 
 class Solution(object):
@@ -19,6 +28,7 @@ class Solution(object):
         """
         low = 0
         high = len(nums) - 1
+
         while low < high:
             med = (low + high) // 2
             if nums[med] < nums[high]:
