dc83db95706f7d978dbe9fc6199c7d0dae432eb4
Aldriech Villamor <81332170+aldriechV@users.noreply.github.com>
2026-01-21 13:39:43 -0800

    Improve comments in Pascal's Triangle implementation
    Refactor comments for clarity and consistency.

--- 119. Pascal's Triangle.py
+++ 119. Pascal's Triangle.py
@@ -9,19 +9,23 @@ class Solution(object):
         :type numRows: int
         :rtype: List[List[int]]
         """
-        # 1. Set up a base case:
+        # Set up a base case:
         if numRows == 0:
             return []
+        # The first row is also treated as a base case since this is the top of the pyramid
         if numRows == 1:
             return [[1]]
         
+        # Memoization allows us to recall the 
         previousRows = self.generate(numRows-1)
-        #creating the last layer here
+        
+        # Creating the newest layer here
         newRow = [1] * numRows
 
+        # Populated the values of each row with the math
         for i in range(1, numRows -1):
             newRow[i] = previousRows[-1][i-1] + previousRows[-1][i]
         
+        #append the new row
         previousRows.append(newRow)
         return previousRows
-    
