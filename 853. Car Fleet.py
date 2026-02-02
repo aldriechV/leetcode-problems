c22da1dc355fec00daec42b830722f77b42dd7d3
Aldriech Villamor <81332170+aldriechV@users.noreply.github.com>
2026-02-02 15:26:43 -0800

    Enhance comments and structure in Car Fleet solution
    Refactor code to improve clarity and add comments.

--- 853. Car Fleet.py
+++ 853. Car Fleet.py
@@ -2,20 +2,11 @@
 Docstring for 853. Car Fleet
 
 There are n cars at given miles away from the starting mile 0, traveling to reach the mile target.
-
 You are given two integer arrays position and speed, both of length n, where position[i] is the starting mile of the ith car and speed[i] is the speed of the ith car in miles per hour.
-
 A car cannot pass another car, but it can catch up and then travel next to it at the speed of the slower car.
-
 A car fleet is a single car or a group of cars driving next to each other. The speed of the car fleet is the minimum speed of any car in the fleet.
-
 If a car catches up to a car fleet at the mile target, it will still be considered as part of the car fleet.
-
 Return the number of car fleets that will arrive at the destination.
-
-
-
-
 """
 
 
@@ -27,13 +18,18 @@ class Solution(object):
         :type speed: List[int]
         :rtype: int
         """
+        # Pair up each car's position and speed for sorting/processing together.
         pair = [[p,s] for p, s in zip(position, speed)]
+
+         # Stack will store arrival times of fleets.
         stack = []
 
+         # Sort by position, then iterate from closest-to-target to farthest.
         for p, s in sorted(pair)[::-1]:
             stack.append(float(target - p) / s)
             
-            #We want this to 
+            # If this car would arrive earlier or same time than the fleet ahead,
+            # it must catch up and merge into that fleet (cannot pass).
             if len(stack) >= 2 and stack[-1] <= stack[-2]:
                 stack.pop()
 
